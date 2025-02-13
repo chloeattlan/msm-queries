33be243470716a7cdbe966997e7692e11e6233ca
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-02-13 20:39:00 +0000

    struggling with grade

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,11 @@
+class DirectorsController < ApplicationController
+  def index
+    render({ :template => "director_templates/list"})
+  end
+  def show
+    an_id = params.fetch("an_id")
+    @matching_records = Director.where({ :id => an_id})
+    @director = @matching_records[0]
+    render({ :template => "director_templates/show"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   get("/", { :controller => "misc", :action => "homepage" })
+  get("/directors", { :controller => "directors", :action => "index" })
+  get("/directors/:an_id", { :controller => "directors", :action => "show" })
 end
