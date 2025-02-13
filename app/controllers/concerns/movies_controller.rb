75fe0730491f0bd03c81cd846f4fe3e6d4763131
chloeattlan <164663007+chloeattlan@users.noreply.github.com>
2025-02-13 22:07:48 +0000

    finished movies

--- app/controllers/concerns/movies_controller.rb
+++ app/controllers/concerns/movies_controller.rb
@@ -4,8 +4,8 @@ class MoviesController < ApplicationController
   end
   def show
     an_id = params.fetch("an_id")
-    @matching_records = Director.where({ :id => an_id})
-    @director = @matching_records[0]
+    @matching_records = Movie.where({ :id => an_id})
+    @movie = @matching_records[0]
     render({ :template => "movie_templates/show"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:an_id", { :controller => "directors", :action => "show" })
   get("/movies/", { :controller => "movies", :action => "index" })
-  get("/movies/", { :controller => "movies", :action => "show" })
+  get("/movies/:an_id", { :controller => "movies", :action => "show" })
 end
